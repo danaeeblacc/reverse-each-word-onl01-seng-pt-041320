cdcd7a2225089961f7286ecd605f42dd43f630b9
Teinelle Charles <teinelledcharles@gmail.com>
2020-04-23 00:38:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
-def reverse_each_word(sentence1)
-  sentence1.each do |sentence1|
-    return "hello there, how are you?"
-  end
+def reverse_each_word(string)
+  convert_to_array = string.split(' ')
+  reversed_words=[]
+  convert_to_array.collect do |string|
+    re
+  reversed_string=reversed_words.join(' ')
 end
-
-  
-  
+versed_words << string.reverse
+  end
