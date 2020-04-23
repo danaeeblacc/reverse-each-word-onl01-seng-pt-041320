8cec00f0cb4e1761be079cfadbebb2f4fd1bbc74
Teinelle Charles <teinelledcharles@gmail.com>
2020-04-23 00:38:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++
@@ -1,9 +0,0 @@
-def reverse_each_word(string)
-  convert_to_array = string.split(' ')
-  reversed_words=[]
-  convert_to_array.collect do |string|
-    re
-  reversed_string=reversed_words.join(' ')
-end
-versed_words << string.reverse
-  end
